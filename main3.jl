6609745b109585827347a664219afb8f44c6314b
Rohit Singh <rohit092002g@gmail.com>
2021-10-20 23:44:23 +0530

    Update main3.jl

--- main3.jl
+++ main3.jl
@@ -10,5 +10,38 @@ else
     println("old")
 end
 
+@printf("true || false= %s\n",true || false ? "true" : "false");
+
+i = 1
+while i < 20
+    if i % 2 == 0
+        println("even", i)
+        # println("even" * 2)#error
+        global i += 1
+        continue # skips rest of the code in this loop
+    end # end if statement
+    global i += 1
+    if i >= 10
+    break
+    end
+end
+
+for j = 1:5
+    println(j)
+end
+
+for k in [1,2,3,4]
+    println(k)
+end
+# println(k)#error, k not defined
+
+for k in 1:5, j in 2:3:20# starts with  with skip of 3
+    println((k, j))
+end
+
+
+
+    
+
 
 
